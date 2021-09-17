f147513fd19d952279ea956e0c5251e70dcd91c7
JuEnPeHa <67208274+JuEnPeHa@users.noreply.github.com>
2021-09-17 15:47:24 -0500

    33 por ciento

---
+++ src/Toggle/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styleToggle.css";
+
+export default function Toggle() {
+    return <div className="toggle">
+        <div className="notch" />
+{/*        <div>
+            <div className="letter one" />
+            <div className="letter two" />
+            <div className="letter three" />
+            <div className="letter four" />
+</div> */}
+        <div className="en"><p>EN</p></div>
+
+
+    </div>
+}
