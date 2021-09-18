ff59c60e725bafbb5cd6093a914722459350b76c
JuEnPeHa <67208274+JuEnPeHa@users.noreply.github.com>
2021-09-18 11:04:35 -0500

    75%

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import React from 'react';
 import Toggle from './Toggle';
-
 import './App.css';
 
 function App() {
+  const [toggled, setToggled] = React.useState(false);
+  const handleClick = () => {
+      setToggled((s) => !s);
+  };
   return (
-    <div className="App">
-      <Toggle />
-
-    </div>
+      <div className="App">
+          <Toggle toggled={toggled} onClick={handleClick} />
+      </div>
   );
 }
 
--- src/Toggle/index.js
+++ src/Toggle/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./styleToggle.css";
 
-export default function Toggle() {
-    return <div className="toggle">
-        <div className="notch" />
-{/*        <div>
-            <div className="letter one" />
-            <div className="letter two" />
-            <div className="letter three" />
-            <div className="letter four" />
-</div> */}
-        <div className="en"><p>EN</p></div>
-
-
+export default function Toggle({toggled, onClick }) {
+    return <div onClick={onClick} className={`toggle${toggled ? " spanish" : " english" }`}>
+        <div className="notch">
+        <div className="en"><p>{`${toggled ? "ES" : "EN" }`}</p></div>
+        </div>
+        <div className="es"><p>{`${toggled ? "EN" : "ES" }`}</p></div>
     </div>
 }
